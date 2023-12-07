31a6f37d5822ed2e47274e2302e3773b90a7b15d
Eaint-Thayaphy-Oo <eaintthayaphyoo098@gmail.com>
2023-12-07 21:24:26 +0630

    myProject commit

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { FontAwesome } from "@expo/vector-icons";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <View></View>
+        <Text style={styles.headerText}>Project 101</Text>
+        {/* <Image
+          source={require("./assets/images/user.jpg")}
+          style={styles.userIcon}
+        /> */}
+        <TouchableOpacity
+          onPress={() => {
+            alert("Hi,this is Touched");
+          }}
+        >
+          <FontAwesome name="user-circle-o" size={30} color="grey" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: StatusBar.currentHeight,
+  },
+
+  header: {
+    width: "100%",
+    backgroundColor: "lightblue",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+  },
+
+  headerText: {
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+
+  userIcon: {
+    width: 30,
+    height: 30,
   },
 });
